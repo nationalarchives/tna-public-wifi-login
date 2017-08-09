4d4aedccf9450c74aa0c7738682647a7322eddfc
Punal Chotrani <punalsc@gmail.com>
2017-08-09 14:35:53 +0100

    refactored layout and checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 $(function() {
-    $('.entry-content a').on('click',function(){
-        $('.overflow').toggleClass( "hide" );
+    $('.entry-content a').on('click',function(e){
+        e.preventDefault();
+        $('.overflow').toggleClass('hide');
+    });
+
+    $('#accept').on('change',function(){
+        if($(this).is(':checked')){
+            $('#connect').prop('disabled', false);
+            $('#connect').attr('aria-disabled', false);
+        } else {
+            $('#connect').prop('disabled', true);
+            $('#connect').attr('aria-disabled', true);
+        }
+    });
+
+    $('#newsletter').on('change', function(){
+       if($(this).is(':checked')){
+           $('.email-signup').removeClass( "hide" );
+       } else {
+           $('.email-signup').addClass( "hide" );
+       }
     });
 });
