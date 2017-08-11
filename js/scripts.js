da11625926bff82b0501ec9a424a106194761abe
Punal Chotrani <punalsc@gmail.com>
2017-08-11 14:26:25 +0100

    added more validations around the checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ $(function() {
      * This is to check show and hide the email field if checked.
     */
     $('#newsletter').on('change', function(){
-       if($(this).is(':checked')){
+       if($(this)[0].checked){
            $('.email-signup').removeClass( "hide" );
        } else {
            $('.email-signup').addClass( "hide" );
@@ -22,6 +22,19 @@ $(function() {
      * This is to remove the required attribute as we only want to use this if there is no JS.
     */
     $('#accept').removeAttr('required');
+
+
+    /*
+     * Check's if the Accept checkbox is checked and hide's the error message.
+    */
+    var onClick = (function () {
+        $("input#accept").on("click", function(){
+            if($(this)[0].checked){
+                $('#accept-error').addClass('hide');
+            }
+        });
+    })();
+
 
     /*
      * Custom validations for checkbox
@@ -35,5 +48,6 @@ $(function() {
             e.preventDefault();
         }
     });
+
 
 });
