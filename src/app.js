2b6ab7362438cce8aa4a64f8b1378533c04029ba
Punal Chotrani <punalsc@gmail.com>
2017-11-30 15:21:22 +0000

    responsive fixes

--- src/app.js
+++ src/app.js
@@ -64,3 +64,6 @@
 //# sourceMappingURL=app.js.map
 //# sourceMappingURL=app.js.map
 //# sourceMappingURL=app.js.map
+//# sourceMappingURL=app.js.map
+//# sourceMappingURL=app.js.map
+//# sourceMappingURL=app.js.map
